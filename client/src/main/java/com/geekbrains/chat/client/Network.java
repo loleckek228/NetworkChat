a7ab7969678ee2c7c6f517ce1904e32f046d0838
Loleckek228 <george-valiev@yandex.ru>
2020-01-23 08:51:37 +0300

    lesson-6

---
+++ client/src/main/java/com/geekbrains/chat/client/Network.java
@@ -0,0 +1,53 @@
+package com.geekbrains.chat.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Network {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public Network(int port) throws IOException {
+        socket = new Socket("localhost", 8189);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+    }
+
+    public String readMsg() throws IOException {
+        return in.readUTF();
+    }
+
+    public void sendMsg(String msg) throws IOException {
+        out.writeUTF(msg);
+    }
+
+    public void close() {
+
+        try {
+            if (in != null) {
+                in.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (out != null) {
+                out.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
